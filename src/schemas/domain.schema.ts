923d85635b16eba6fa22ac040086976bdcc4dd88
sagiv bonali <sagivb13@gmail.com>
2023-05-18 18:07:32 +0300

    Add virusTotal and whoIs for schema

--- src/schemas/domain.schema.ts
+++ src/schemas/domain.schema.ts
@@ -1,12 +1,18 @@
 import { Prop, SchemaFactory, Schema } from '@nestjs/mongoose';
-import { HydratedDocument, ObjectId, Types } from 'mongoose';
+import { HydratedDocument } from 'mongoose';
 
 export type DomainDocument = HydratedDocument<Domain>;
 
 @Schema()
 export class Domain {
-  @Prop({ required: true, unique: true, index: true })
+  @Prop({ required: true, unique: true, index: true, type: String })
   path: string;
+
+  @Prop({ type: Object })
+  virusTotal?: object;
+
+  @Prop({ type: Object })
+  whoIs?: object;
 }
 
 export const DomainSchema = SchemaFactory.createForClass(Domain);

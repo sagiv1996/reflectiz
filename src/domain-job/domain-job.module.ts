67db2c4b7cfda412d122d185fba8edab6c83bafe
sagiv bonali <sagivb13@gmail.com>
2023-05-19 10:27:32 +0300

    support with whoIs api

--- src/domain-job/domain-job.module.ts
+++ src/domain-job/domain-job.module.ts
@@ -3,9 +3,20 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { DomainJobService } from './domain-job.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Domain, DomainSchema } from 'src/schemas/domain.schema';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    HttpModule.register({
+      maxRedirects: 3,
+      baseURL: process.env.WHO_IS_URL,
+      params: {
+        apiKey: process.env.WHO_IS_API_KEY,
+        outputFormat: 'json',
+      },
+    }),
     MongooseModule.forFeature([{ name: Domain.name, schema: DomainSchema }]),
     ScheduleModule.forRoot(),
   ],
--- src/domain-job/domain-job.service.ts
+++ src/domain-job/domain-job.service.ts
@@ -1,17 +1,50 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { AxiosResponse } from 'axios';
+import { query } from 'express';
 import { Model } from 'mongoose';
-import { Domain } from 'src/schemas/domain.schema';
+import { Observable, firstValueFrom } from 'rxjs';
+import { Domain, DomainDocument } from 'src/schemas/domain.schema';
 
 @Injectable()
 export class DomainJobService {
-  constructor(@InjectModel(Domain.name) private domainModel: Model<Domain>) {}
+  constructor(
+    @InjectModel(Domain.name) private domainModel: Model<Domain>,
+    private readonly httpService: HttpService,
+  ) {}
 
   @Cron(CronExpression.EVERY_5_SECONDS)
-  async handleCron() {
-    const domains = await this.domainModel.find();
+  async updateNewDomains() {
     console.debug('Executing cron job...');
-    // Add your custom logic here
+    const domains = await this.domainModel.find({
+      virusTotal: null,
+      whoIs: null,
+    });
+    console.debug(`Found ${domains.length} domains to update.`);
+
+    const updatePromisesArray = [];
+
+    for (const domain of domains) {
+      updatePromisesArray.push(this.updateWhoIs(domain));
+    }
+
+    await Promise.allSettled(updatePromisesArray);
+  }
+
+  private async getDataFromWhoIs(path: string) {
+    const response = this.httpService.get('', {
+      params: {
+        domainName: path,
+      },
+    });
+    return firstValueFrom(response);
+  }
+
+  private async updateWhoIs(domain: DomainDocument) {
+    const { data } = await this.getDataFromWhoIs(domain.path);
+    domain.whoIs = data;
+    await domain.save();
   }
 }
